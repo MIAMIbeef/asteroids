5c5fb7e31d1dc6a22c59facbc626d80e00297fe1
MIAMIbeef <jptaylor5879@gmail.com>
2025-01-02 20:04:47 -0500

    circleshape.py

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ def main():
     # screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # define FPS
+    clock = pygame.time.Clock()
+    # delta time
+    dt = 0
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
-
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
