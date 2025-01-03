814be033a49e263ad14c780a7bca52391b3c5ad3
MIAMIbeef <jptaylor5879@gmail.com>
2025-01-02 19:55:40 -0500

    game loop and .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+
+    # welcome message and screen width and height
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
