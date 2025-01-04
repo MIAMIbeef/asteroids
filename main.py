72ef728d4b67b021e2966e18f1cebc766a6229d5
MIAMIbeef <jptaylor5879@gmail.com>
2025-01-03 19:34:43 -0500

    asteroid rotation

--- main.py
+++ main.py
@@ -19,16 +19,19 @@ def main():
     # delta time
     dt = 0
 
+    player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT / 2))
+    
     # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT / 2))
-        player.draw(screen)
+        player.update(dt)
+        player.draw(screen)     
         pygame.display.flip()
-        dt = (clock.tick(60) / 1000)
+        tick = clock.tick(60)
+        dt = tick/1000
 
 if __name__ == "__main__":
     main()
